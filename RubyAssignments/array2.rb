59c372addffd6b55caff0800bd0942a514f7c56c
kalpana <ksharma@grepruby.com>
2017-08-23 19:54:29 +0530

    code modified

--- RubyAssignments/array2.rb
+++ RubyAssignments/array2.rb
@@ -1,16 +1,28 @@
 #!user/bin/ruby
 
-array1 =Array.new(5) {|e| e = e*10+1 }
+array1 =Array.new(5) { |e| e = e * 10 + 1 }
 array2 =Array(0..9)
-array3 =Array.new(4,"mac")
+array3 =Array.new(4, "mac")
 combine_array= Array.new
 
 combine_array = array1 + array2 + array3
 
-puts "#{array1}, #{array2}, #{array3} \n #{combine_array}"
+puts " First Array is : \n #{array1} \n\n Second Array is : \n #{array2} \n\n Third Array is : \n #{array3} \n\n Combined Array is : \n #{combine_array}"
+
 
 =begin
 output:
-[1, 11, 21, 31, 41], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ["mac", "mac", "mac", "mac"] 
+
+First Array is : 
+ [1, 11, 21, 31, 41] 
+
+ Second Array is : 
+ [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] 
+
+ Third Array is : 
+ ["mac", "mac", "mac", "mac"] 
+
+ Combined Array is : 
  [1, 11, 21, 31, 41, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "mac", "mac", "mac", "mac"]
+
 =end
