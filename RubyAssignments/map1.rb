0a8298551ef00ec3251c671831a05c4df0677331
kalpana <ksharma@grepruby.com>
2017-08-23 20:35:13 +0530

    modified code

--- RubyAssignments/map1.rb
+++ RubyAssignments/map1.rb
@@ -1,18 +1,31 @@
 #!user/bin/ruby
 
-array = Array(0...6)
+sample_array = Array(0...6)
 
-puts "#{array}"
+class Mapping
+	def map_array(arr)
 
-array= array.map{|i| i*2}
+		array = arr
 
-puts "#{array}"
+		puts " Sample array is : #{array} "
 
+		output_array= array.map { |i| i * 2}
+
+    puts " Output array is : #{output_array}"
+
+	end
+end
+
+doubled_array = Mapping.new
+doubled_array.map_array(sample_array)
+
+print "\n"
 
 =begin
 output:
 
-[0, 1, 2, 3, 4, 5]
-[0, 2, 4, 6, 8, 10]
+Sample array is : [0, 1, 2, 3, 4, 5] 
+ Output array is : [0, 2, 4, 6, 8, 10]
+
 
 =end
