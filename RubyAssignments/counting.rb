a57d09af35d1711f6021155fcaf983c18c658660
kalpana <ksharma@grepruby.com>
2017-08-23 20:16:27 +0530

    modified code

--- RubyAssignments/counting.rb
+++ RubyAssignments/counting.rb
@@ -7,10 +7,10 @@ class Counting
 	  
 	  while count <= n
 	  
-		  print " #{count}" # Will print counting in a row
+		  print " #{ count } " # Will print counting in a row
 		  # puts count      #Will print counting in a column
 		  
-		  count +=1
+		  count += 1
 		  
 	  end
 	  
@@ -19,7 +19,7 @@ class Counting
   end
 end
 c = Counting.new
-c.print_count(80)
+c.print_count (80)
 print "\n"
 
 # Another way to print counting
@@ -35,5 +35,4 @@ end
 =begin
 
 output :
-1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 
-=end
+ 1  2  3  4  5  6  7  8  9  10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26  27  28  29  30  31  32  33  34  35  36  37  38  39  40  41  42  43  44  45  46  47  48  49  50  51  52  53  54  55  56  57  58  59  60  61  62  63  64  65  66  67  68  69  70  71  72  73  74  75  76  77  78  79  80 
