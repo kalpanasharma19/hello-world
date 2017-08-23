48b97fbd909b2ff5e590a4ddd85d1ccaca2055dd
kalpanasharma19 <31214770+kalpanasharma19@users.noreply.github.com>
2017-08-23 18:18:06 +0530

    added output

--- RubyAssignments/array2.rb
+++ RubyAssignments/array2.rb
@@ -8,3 +8,9 @@ combine_array= Array.new
 combine_array = array1 + array2 + array3
 
 puts "#{array1}, #{array2}, #{array3} \n #{combine_array}"
+
+=begin
+output:
+[1, 11, 21, 31, 41], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], ["mac", "mac", "mac", "mac"] 
+ [1, 11, 21, 31, 41, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "mac", "mac", "mac", "mac"]
+=end
